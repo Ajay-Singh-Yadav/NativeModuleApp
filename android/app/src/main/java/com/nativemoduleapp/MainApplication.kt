76976d4b0b2e936d67b8e35267bf2dca0cd12377
Ajay Singh Yadav <ajayyadav442408@gmail.com>
2025-06-28 14:47:51 +0530

    connected

--- android/app/src/main/java/com/nativemoduleapp/MainApplication.kt
+++ android/app/src/main/java/com/nativemoduleapp/MainApplication.kt
@@ -1,5 +1,7 @@
 package com.nativemoduleapp
 
+
+
 import android.app.Application
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
@@ -9,6 +11,8 @@ import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
+import com.nativelocalstorage.NativeLocalStoragePackage
+
 
 class MainApplication : Application(), ReactApplication {
 
@@ -18,6 +22,10 @@ class MainApplication : Application(), ReactApplication {
             PackageList(this).packages.apply {
               // Packages that cannot be autolinked yet can be added manually here, for example:
               // add(MyReactNativePackage())
+               add(NativeLocalStoragePackage())
+              
+            
+
             }
 
         override fun getJSMainModuleName(): String = "index"
